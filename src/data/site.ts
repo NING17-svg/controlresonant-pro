8fabe1b2fb7bb2a8814eae8276c127900eb1be13
ningshiqi017 <ningxingfu0429@gmail.com>
2026-09-19 11:29:10 +0800

    ga4-connector: bake GA4 measurement ID into site config for static export

--- src/data/site.ts
+++ src/data/site.ts
@@ -54,7 +54,7 @@ export const site: SiteConfig = {
     },
   ],
   author: "CONTROL Resonant Guide",
-  gaMeasurementId: process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || "",
+  gaMeasurementId: "G-LD9YZXL5CX",
   bingSiteAuthCode: process.env.NEXT_PUBLIC_BING_SITE_AUTH_CODE || "",
   officialSources: [
     {
